334866805e3b3cfbd09aa8216315b14a33c01f62
Dazzrr <appsenence@outlook.com>
2023-04-19 11:27:42 +0200

    Version 1.0

--- ani.js
+++ ani.js
@@ -110,8 +110,7 @@ function setAnimation(elementID, animation, time_ms, reversed, delayPerChar_ms, 
 
 function turnTextToSpans(string) {
     return Array.from(string).map(char => {
-        if(char !== " ") return `<span style="position: relative; display: inline-block;" class="char-text-animation-library">${char}</span>`;
-        return `<span style="position: relative; display: inline-block; min-width: 7px;" class="char-text-animation-library">${char}</span>`;
+        return `<span style="position: relative; display: inline-block; min-width: 5px;" class="char-text-animation-library">${char}</span>`;
     });
 }
 
